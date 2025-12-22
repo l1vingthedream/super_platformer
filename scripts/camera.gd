17da1092e7b6088ac24bf7086b6f108050376ef4
l1vingthedream <mikebegic@gmail.com>
2025-12-22 09:53:16 -0500

    Add camera script to lock ground to bottom of display

---
+++ scripts/camera.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+@onready var target: Node2D = get_parent().get_node("Player")
+
+var fixed_y: float
+
+func _ready():
+	var viewport_height = get_viewport_rect().size.y
+	var ground_bottom = 1032.0
+	fixed_y = ground_bottom - (viewport_height / 2.0 / zoom.y)
+
+func _process(_delta):
+	if target:
+		global_position.x = target.global_position.x
+		global_position.y = fixed_y

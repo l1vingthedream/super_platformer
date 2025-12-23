0c829680cec8daa53c583238f1cbee9e9e9442d9
l1vingthedream <mikebegic@gmail.com>
2025-12-22 21:11:04 -0500

    Restore camera ground lock for TileMapLayer
    Lock ground to bottom of display with ground_bottom = 0.0 for new tile layout

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,6 +2,14 @@ extends Camera2D
 
 @onready var target: Node2D = get_parent().get_node("Player")
 
+var fixed_y: float
+
+func _ready():
+	var viewport_height = get_viewport_rect().size.y
+	var ground_bottom = 0.0
+	fixed_y = ground_bottom - (viewport_height / 2.0 / zoom.y)
+
 func _process(_delta):
 	if target:
-		global_position = target.global_position
+		global_position.x = target.global_position.x
+		global_position.y = fixed_y
